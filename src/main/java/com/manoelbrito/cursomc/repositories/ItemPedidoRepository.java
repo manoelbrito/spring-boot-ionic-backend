5d1df2177d8209aea6bd850ddd533bf05d283f43
Manoel Brito <manoelbrito05@gmail.com>
2018-10-30 11:56:40 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/manoelbrito/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.manoelbrito.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.manoelbrito.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
