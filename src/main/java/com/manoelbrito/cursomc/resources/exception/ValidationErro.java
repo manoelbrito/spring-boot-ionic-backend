dc89fffe1bf0319b84b96bdfd54f88ea7bb4ae6e
Manoel Brito <manoelbrito05@gmail.com>
2018-11-03 10:14:23 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/manoelbrito/cursomc/resources/exception/ValidationErro.java
@@ -0,0 +1,23 @@
+package com.manoelbrito.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationErro extends StrandardError {
+
+	
+	private static final long serialVersionUID = 1L;
+	private List<FieldMessage> errors=new ArrayList<>();
+	
+	public ValidationErro(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		
+	}
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+	public void addError(String name, String message) {
+		errors.add(new FieldMessage(name, message) );
+	}
+	
+}
