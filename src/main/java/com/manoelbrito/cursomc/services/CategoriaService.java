3fd222c2191ed7537527bf5969e58ae4daab9850
Manoel Brito <manoelbrito05@gmail.com>
2018-11-04 11:40:42 -0200

    Update em CategoriaServices e construtor de pagamento

--- src/main/java/com/manoelbrito/cursomc/services/CategoriaService.java
+++ src/main/java/com/manoelbrito/cursomc/services/CategoriaService.java
@@ -34,10 +34,11 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
-	public Categoria update(Categoria obj) {
+public Categoria update(Categoria obj) {
 		
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj =find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -64,6 +65,12 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+		
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	
 		
 	}
 }
