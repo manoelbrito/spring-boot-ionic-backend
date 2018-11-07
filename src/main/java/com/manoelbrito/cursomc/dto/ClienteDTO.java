363315ef85dcd0f31ba80aea585de59d374ede66
Manoel Brito <manoelbrito05@gmail.com>
2018-11-07 12:37:24 -0200

    Validação customizada de email não repetido na atualização de Cliente

--- src/main/java/com/manoelbrito/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/manoelbrito/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.manoelbrito.cursomc.domain.Cliente;
+import com.manoelbrito.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
