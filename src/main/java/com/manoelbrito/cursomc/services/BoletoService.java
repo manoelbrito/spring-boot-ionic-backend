dfee4eccb3b8ead70f9ecbdc373f4bb91b54a3f8
Manoel Brito <manoelbrito05@gmail.com>
2018-11-10 20:13:30 -0200

    Inserindo Pedido

---
+++ src/main/java/com/manoelbrito/cursomc/services/BoletoService.java
@@ -0,0 +1,20 @@
+package com.manoelbrito.cursomc.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.manoelbrito.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pgto, Date instanteDoPedido) {
+		Calendar cal=Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pgto.setDataVencimento(cal.getTime());
+		
+	}
+}
--- src/main/java/com/manoelbrito/cursomc/services/PedidoService.java
+++ src/main/java/com/manoelbrito/cursomc/services/PedidoService.java
@@ -1,12 +1,19 @@
 package com.manoelbrito.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.manoelbrito.cursomc.domain.ItemPedido;
+import com.manoelbrito.cursomc.domain.PagamentoComBoleto;
 import com.manoelbrito.cursomc.domain.Pedido;
+import com.manoelbrito.cursomc.domain.enums.EstadoPagamento;
+import com.manoelbrito.cursomc.repositories.ItemPedidoRepository;
+import com.manoelbrito.cursomc.repositories.PagamentoRepository;
 import com.manoelbrito.cursomc.repositories.PedidoRepository;
+import com.manoelbrito.cursomc.repositories.ProdutoRepository;
 import com.manoelbrito.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -14,6 +21,18 @@ public class PedidoService {
 	
 	@Autowired
 	private PedidoRepository repo;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public Pedido find(Integer id) {
 		
@@ -21,5 +40,30 @@ public class PedidoService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 		
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstate(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pgto=(PagamentoComBoleto)obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pgto, obj.getInstate());
+			
+		}
+		obj=repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		
+		for(ItemPedido ip: obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoRepository.findById(ip.getProduto().getId()).get().getPreco());
+			ip.setPedido(obj);
+			
+			
+			
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
