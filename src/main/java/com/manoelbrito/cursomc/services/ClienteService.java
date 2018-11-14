237a322242d97789bd1ad818588fe9a22b1779b0
Manoel Brito <manoelbrito05@gmail.com>
2018-11-14 16:36:39 -0400

    Adcionando senha ao Cliente

--- src/main/java/com/manoelbrito/cursomc/services/ClienteService.java
+++ src/main/java/com/manoelbrito/cursomc/services/ClienteService.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,6 +31,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 
 	public Cliente find(Integer id) {
 		
@@ -79,12 +83,12 @@ public Cliente update(Cliente obj) {
 	}
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
-		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
 		
 	}
 	
 	public Cliente fromDTO(ClienteNewDTO objNewDto) {
-		 Cliente cli=new Cliente(null, objNewDto.getNome(), objNewDto.getEmail(), objNewDto.getCpfOuCnpj(), TipoCliente.toEnum(objNewDto.getTipo()));
+		 Cliente cli=new Cliente(null, objNewDto.getNome(), objNewDto.getEmail(), objNewDto.getCpfOuCnpj(), TipoCliente.toEnum(objNewDto.getTipo()), pe.encode(objNewDto.getSenha()));
 		 Cidade cid= new  Cidade(objNewDto.getCidadeId(), null, null);
 		 Endereco end=new Endereco(null,objNewDto.getLogradouro(), objNewDto.getNumero(), objNewDto.getComplemento(), objNewDto.getBairro(), objNewDto.getCep(), cli, cid );
 		 cli.getEnderecos().add(end);
