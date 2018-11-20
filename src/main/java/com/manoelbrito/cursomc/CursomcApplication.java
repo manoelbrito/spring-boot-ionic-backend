06d248a9b3b269c1a4306be98e632c1abc4579da
Manoel Brito <manoelbrito05@gmail.com>
2018-11-20 16:07:18 -0400

    Salvando primeiro arquivo no S3

--- src/main/java/com/manoelbrito/cursomc/CursomcApplication.java
+++ src/main/java/com/manoelbrito/cursomc/CursomcApplication.java
@@ -1,18 +1,25 @@
 package com.manoelbrito.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.manoelbrito.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.UploadFile("C:\\Users\\Manoel\\Desktop\\Curso Spring Boot\\foto.jpg");
+		
 	}
 }
